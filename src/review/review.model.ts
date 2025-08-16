d04aac2fc1eebdd362dc256192b8f5515e044f88
Dmytro Oliinyk <prototupe@ukr.net>
2025-08-16 09:42:01 +0300

    added review service & tests

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,18 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unsafe-declaration-merging */
+import { prop } from '@typegoose/typegoose';
+import { Base } from '@typegoose/typegoose/lib/defaultClasses';
+import { Types } from 'mongoose';
+
+export interface ReviewModel extends Base {}
 export class ReviewModel {
-  _id: string;
+  @prop()
   name: string;
+  @prop()
   title: string;
+  @prop()
   description: string;
+  @prop()
   rating: number;
-  createdAt: Date;
+  productId: Types.ObjectId;
 }
---
+++ src/review/review.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ReviewModel } from './review.model';
+import type { ModelType, DocumentType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from '@m8a/nestjs-typegoose';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { DeleteResult, Types } from 'mongoose';
+
+@Injectable()
+export class ReviewService {
+  constructor(
+    @InjectModel(ReviewModel)
+    private readonly reviewModel: ModelType<ReviewModel>,
+  ) {}
+
+  async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
+    return this.reviewModel.create(dto);
+  }
+
+  async delete(id: string): Promise<DocumentType<ReviewModel> | null> {
+    return this.reviewModel.findByIdAndDelete(id).exec();
+  }
+  async findByProductId(
+    productId: string,
+  ): Promise<DocumentType<ReviewModel>[]> {
+    return this.reviewModel
+      .find({ productId: new Types.ObjectId(productId) })
+      .exec();
+  }
+
+  async deleteByProductId(productId: string): Promise<DeleteResult> {
+    return this.reviewModel
+      .deleteMany({ productId: new Types.ObjectId(productId) })
+      .exec();
+  }
+}
